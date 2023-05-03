8a6a18694dc6d4a0bb77862448a7e8f95bcea169
raju720190r <raju720190r@gmail.com>
2023-05-04 00:56:01 +0600

    Footer Section

---
+++ components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+            <h2 className='text-5xl font-bold'>Footer Section</h2>
+        </div>
+    );
+};
+
+export default Footer;
